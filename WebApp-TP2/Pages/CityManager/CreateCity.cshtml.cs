722ba72376bcd6b64d3d917fb3290484a2eef6f7
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-14 12:00:40 -0300

    Adicionar arquivos de projeto.

---
+++ WebApp-TP2/Pages/CityManager/CreateCity.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_TP2.Pages.CityManager
+{
+    public class CreateCityModel : PageModel
+    {
+        [BindProperty]
+        public string cityName { get; set; }
+    }
+}
