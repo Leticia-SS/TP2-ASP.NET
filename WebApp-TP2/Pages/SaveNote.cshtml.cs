0723932d722e068a8b88d032c6bf1301ec7df10e
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-19 18:20:51 -0300

    exercicio 10

--- WebApp-TP2/Pages/SaveNote.cshtml.cs
+++ WebApp-TP2/Pages/SaveNote.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,29 @@ namespace WebApp_TP2.Pages
 {
     public class SaveNoteModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string DownloadLink { get; set; }
+
+        public class InputModel
         {
+            [Required]
+            public string Content { get; set; }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var fileName = $"note-{DateTime.Now:yyyyMMddHHmmss}.txt";
+            var filePath = Path.Combine("wwwroot/files", fileName);
+
+            Directory.CreateDirectory("wwwroot/files");
+            System.IO.File.WriteAllText(filePath, Input.Content);
+
+            DownloadLink = $"/files/{fileName}";
+            return Page();
         }
     }
 }
