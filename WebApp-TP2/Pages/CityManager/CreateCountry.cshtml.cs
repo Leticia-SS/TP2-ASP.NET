24cea3a9e88d5911f48a82d14ee0e07c76be2693
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-19 17:32:47 -0300

    exercicio 07

--- WebApp-TP2/Pages/CityManager/CreateCountry.cshtml.cs
+++ WebApp-TP2/Pages/CityManager/CreateCountry.cshtml.cs
@@ -7,25 +7,31 @@ namespace WebApp_TP2.Pages.CityManager
     public class CreateCountryModel : PageModel
     {
         [BindProperty]
-        public InputModel Input { get; set; }
+        public List<InputModel> Countries { get; set; } = new List<InputModel>();
+
+        public void OnGet()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Countries.Add(new InputModel());
+            }
+        }
+
+        public void OnPost()
+        {
+            var countryList = Countries.Select(c => new Country
+            {
+                CountryName = c.CountryName,
+                CountryCode = c.CountryCode
+            }).ToList();
+        }
 
         public class InputModel
         {
             public string CountryName { get; set; }
 
-            [StringLength(2, MinimumLength = 2, ErrorMessage = "O código deve ter exatamente 2 caracteres")]
+            [StringLength(2, MinimumLength = 2)]
             public string CountryCode { get; set; }
         }
-
-        public void OnPost()
-        {
-            var country = new Country
-            {
-                CountryName = Input.CountryName,
-                CountryCode = Input.CountryCode
-            };
-        }
-
-    
     }
 }
